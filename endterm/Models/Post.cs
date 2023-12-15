c49749587020ebef741e62c328007ffaf778c90d
Kuandykova Sara <kuandykovasara29@gmail.com>
2023-12-15 20:09:30 +0600

    added model Post

---
+++ endterm/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿namespace endterm.Models;
+
+public class Post
+{
+    public int Id { get; init; }
+    public string Title { get;  init; }
+    public string Text { get; init; }
+    public string Author { get; init; }
+    public DateTime CreatedAt { get; init; }
+
+    public Post(
+        int id, 
+        string title,
+        string text,
+        string author
+        )
+    {
+        Id = id;
+        Title = title;
+        Text = text;
+        Author = author;
+        CreatedAt = DateTime.UtcNow;
+    }
+    
+    private Post(){}
+}
